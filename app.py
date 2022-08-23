971256ca5b20bac26f559764319ef62f3ca55a9a
My name <me@example.com>
2022-08-23 07:04:50 +0700

    image display update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 @st.cache()
 def get_thresh(image):
@@ -26,24 +27,24 @@ def get_contours(thresh_img):
 
 @st.cache()
 def count_dots(contours, card):
-    xs = np.array([contour.shape[0] for contour in contours])
-    
-    if len(contours)==2 or np.std(xs)<2:
-        return 0
+    xs = np.array([contour.shape[0] for contour in contours]) # contour.shape[0]
+
+    if len(contours)<=2 or np.std(xs)<2:
+        return 0, None
 
     # Where the input image is only one card
     if card == 'Single':
         xs_mean = np.mean(xs)
-        xs_dot = xs[xs < xs_mean]
+        dot_contours = [contour for contour in contours if contour.shape[0] < xs_mean]
 
     # Use the tolerance value if the input image is more than one card
     else:
         tolerance = 3
         most_freq = max(set(list(xs)), key=list(xs).count)
         dot_ranges = range(most_freq, most_freq+tolerance)
-        xs_dot = [x_dot for x_dot in xs if x_dot in dot_ranges]
+        dot_contours = [contour for contour in contours if contour.shape[0] in dot_ranges]
 
-    return len(xs_dot)
+    return len(dot_contours), dot_contours
 
 st.title('Dot Counter')
 
@@ -87,7 +88,7 @@ elif app_mode == 'Run the app':
         # Dashboard
         thresh_image = get_thresh(image)
         contours = get_contours(thresh_image)
-        dots = count_dots(contours, card)
+        dots, dot_contours = count_dots(contours, card)
 
         dots_text.write(
             f"<h1 style='text-align: center; color: green;'>{dots}</h1>", 
@@ -98,10 +99,16 @@ elif app_mode == 'Run the app':
         image_copy = image.copy()
         out_image = cv2.drawContours(
             image=image_copy, 
-            contours=contours, 
+            contours=dot_contours, 
             contourIdx=-1, 
             color=(0, 255, 0),
             thickness=2,
             lineType=cv2.LINE_AA
         )
-        st.image(out_image,use_column_width= True)
+        p = plt.figure(figsize=(10, 10))
+        plt.imshow(cv2.cvtColor(out_image, cv2.COLOR_BGR2RGB)[:,:,::-1])
+        plt.axis('off')
+
+        st.pyplot(p)
+
+        #st.image(out_image,use_column_width= True)
